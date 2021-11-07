097aab4ae17d479cbd052093011e230ab88884d4
Emma McAvoy <elgmcavoy@gmail.com>
2021-11-07 16:16:15 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Home Page'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+app.register_error_handler(400, handle_bad_request)
