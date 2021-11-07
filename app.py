e9a4596732ae37181a8f20559e34cae9ad0db761
Emma McAvoy <elgmcavoy@gmail.com>
2021-11-07 17:18:16 -0500

    Edited app.py to extract ip address and interface names

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Home Page'
+
 @app.route('/interfaces')
 def interfaces():
-    return 'interfaces reached'
+    interfaces = {}
 
+    with open('interfaces.txt', 'rt') as file:
+        for line in file:
+            link = line.find('Link ')
+            if (link != -1):
+                linky = line.split()
+
+            k = line.find('addr:')
+            if (k != -1):
+                conv = line[k:].split()[0].split(':')[1]
+
+            interfaces[linky[0]] = conv
+
+    return interfaces
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
