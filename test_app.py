fff534c3e048395250c26531baa3c211712fea6d
Emma McAvoy <elgmcavoy@gmail.com>
2021-12-03 15:04:55 -0500

    cahnges necessary for testing to work properly

--- test_app.py
+++ test_app.py
@@ -8,7 +8,8 @@ class TestApp():
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         assert(response.status_code  == 200)
-        assert(response.data  == b'Default Home Page')  
+        assert(response.data  == b'Home Page')  
 
-
-   
+if __name__ == "__main__":
+    ta = TestApp()
+    ta.test_index()
